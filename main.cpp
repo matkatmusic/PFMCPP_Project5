38cee4b425033872ee7baf545165dc22706135ef
matkatmusic <matkatmusic@gmail.com>
2020-01-14 20:33:02 +0000

    Added Part1 Instructions

--- main.cpp
+++ main.cpp
@@ -1,7 +1,54 @@
-#include <iostream>
+/*
+Project 5: Part 1 / 4
+ video Chapter 2 - Part 12
 
+ Create a branch named Part1
+
+ Destructors
+ 
+ 1) Copy a few of your user-defined types (the ones with constructors and for()/while() loops from Project 3) here
+ 2) add destructors
+        make the destructors do something like print out the name of the class.
+
+	If you already did Chapter2 part 10 on Coliru and don't have a Project3, just grab some UDTs from your Coliru Ch2P10 submission. 
+ 
+ 3) add 2 new UDTs that use only the types you copied above.
+ 4) Add destructors to these 2 new types that do stuff.  
+        maybe print out the name of the class being destructed, or call a member function of one of the members.  be creative
+ 
+ 5) Instantiate your 2 UDT's from step 4) in the main() function at the bottom.
+ 
+ 6) After you finish, click the [run] button.  Clear up any errors or warnings as best you can.
+ 
+ Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
+ 
+ Send me the the link to your repl.it in a DM on Slack
+
+ Wait for my code review.
+ */
+
+/*
+ copied UDT 1:
+ */
+
+/*
+ copied UDT 2:
+ */
+
+/*
+ copied UDT 3:
+ */
+
+/*
+ new UDT 4:
+ */
+
+/*
+ new UDT 5:
+ */
+
+#include <iostream>
 int main()
 {
     std::cout << "good to go!" << std::endl;
-    return 0;    
 }
