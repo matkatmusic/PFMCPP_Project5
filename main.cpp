b3d2c7394330e2dad56efb46d7516e513fda45f7
Charles Schiermeyer <matkatmusic@gmail.com>
2020-03-30 20:16:34 -0700

    added part1 instructions

--- main.cpp
+++ main.cpp
@@ -1,7 +1,72 @@
-#include <iostream>
+/*
+Project 5: Part 1 / 4
+ video Chapter 2 - Part 12
 
+ Create a branch named Part1
+
+Purpose:  This project continues developing Project3 and will take you through how to write code that doesn't leak as well as how to refactor. 
+
+ Destructors
+
+ If you didn't do Project3, write 3 UDTs below (where it asks for Copied UDTs) that have: 
+        member functions,
+        member variables, 
+        constructors that initialize the members. 
+        for() or while() loops that modify member variables
+        
+ Define your implementations of all functions OUTSIDE of the class. 
+ Then do instruction 2)
+ 
+ If you DID do Project 3, do instruction 1)
+ 
+ 1) Copy a few of your user-defined types (the ones with constructors and for()/while() loops from Project 3) here
+ 2) add destructors
+        make the destructors do something like print out the name of the class.
+
+	If you already did Chapter2 part 10 on Coliru and don't have a Project3, just grab some UDTs from your Coliru Ch2P10 submission. 
+ 
+ 3) add 2 new UDTs that use only the types you copied above.
+ 4) Add destructors to these 2 new types that do stuff.  
+        maybe print out the name of the class being destructed, or call a member function of one of the members.  be creative
+ 
+ 5) copy over your main() from the end of Project3 and get it to compile with the types you copied over.
+        remove any code in main() that uses types you didn't copy over.
+ 
+ 6) Instantiate your 2 UDT's from step 4) in the main() function at the bottom.
+ 
+ 7) After you finish, click the [run] button.  Clear up any errors or warnings as best you can.
+ 
+ Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
+ 
+Make a pull request after you make your first commit and pin the pull request link to our DM thread.
+
+send me a DM to check your pull request
+
+ Wait for my code review.
+ */
+
+/*
+ copied UDT 1:
+ */
+
+/*
+ copied UDT 2:
+ */
+
+/*
+ copied UDT 3:
+ */
+
+/*
+ new UDT 4:
+ */
+
+/*
+ new UDT 5:
+ */
+
+#include <iostream>
 int main()
 {
     std::cout << "good to go!" << std::endl;
-    return 0;    
 }
