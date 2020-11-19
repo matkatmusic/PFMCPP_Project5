672de1e9070b1186b84e896a2c93c99662b15b69
matkatmusic <matkatmusic@gmail.com>
2020-11-19 03:29:33 +0000

    added part 3

--- main.cpp
+++ main.cpp
@@ -1,66 +1,52 @@
 /*
-Project 5: Part 2 / 4
- video: Chapter 3 Part 1
+ Project 5: Part 3 / 4
+ video: Chapter 3 Part 4: 
 
-Create a branch named Part2
+Create a branch named Part3
 
- The 'this' keyword
- 
- The purpose of this project part is to show you how accessing member variables of objects INSIDE member functions is very similar to accessing member variables of objects OUTSIDE of member functions, via 'this->' and via the '.' operator.
- This project part will break the D.R.Y. rule, but that is fine for the purpose of this project part.
- 
- 
- 1) if you don't have any std::cout statements in main() that access member variables of your U.D.Ts
-         write some.
-    You can copy some from your Project3's main() if needed.
- 
- 2) For each std::cout statement in main() that accessed member variables of your types or printed out the results of your member function calls,
-        a) write a member function that prints the same thing out, but uses the proper techniques inside the member functions to access the same member variables/functions.
-        b) be explicit with your use of 'this->' in those member functions so we see how you're accessing/calling those member variables and functions *inside*
-        c) call that member function after your std::cout statement in main.
-        d) you should see 2 (almost) identical messages in the program output for each member function you add:
-            one for the std::cout line, and one for the member function's output
- 
- 
- 3) After you finish, click the [run] button.  Clear up any errors or warnings as best you can.
- */
+ the 'new' keyword
 
-/*
- example:
- */
-#include <iostream>
-namespace Example
-{
-    //a User-Defined Type
-    struct MyFoo
-    {
-        MyFoo() { std::cout << "creating MyFoo" << std::endl; }
-        ~MyFoo() { std::cout << "destroying MyFoo" << std::endl; }
-		
-		// 2a) the member function whose function body is almost identical to the std::cout statement in main.
-        void memberFunc() 
-		{ 
-            // 2b) explicitly using 'this' inside this member function.
-			std::cout << "MyFoo returnValue(): " << this->returnValue() << " and MyFoo memberVariable: " << this->memberVariable << std::endl; 
-		}  
-		
-        int returnValue() { return 3; }
-        float memberVariable = 3.14f;
-    };
+ 1) add #include "LeakedObjectDetector.h" to main
+ 
+ 3) Add 'JUCE_LEAK_DETECTOR(OwnerClass)' at the end of your UDTs.
+ 
+ 4) write the name of your class where it says "OwnerClass"
+ 
+ 5) write wrapper classes for each type similar to how it was shown in the video
+ 
+ 7) update main() 
+      replace your objects with your wrapper classes, which have your UDTs as pointer member variables.
+      
+    This means if you had something like the following in your main() previously: 
+*/
+#if false
+ Axe axe;
+ std::cout << "axe sharpness: " << axe.sharpness << "\n";
+ #endif
+ /*
+    you would update that to use your wrappers:
     
-    int main()
-    {
-        //an instance of the User-Defined Type named mf
-        MyFoo mf;
-		
-        // 1) a std::cout statement that uses mf's member variables
-        std::cout << "mf returnValue(): " << mf.returnValue() << " and mf memberVariable: " << mf.memberVariable << std::endl; 
-		
-        // 2c) calling mf's member function.  the member function's body is almost identical to the cout statement above.
-        mf.memberFunc();
-        return 0;
-    }
-}
+ */
+
+#if false
+AxeWrapper axWrapper( new Axe() );
+std::cout << "axe sharpness: " << axWrapper.axPtr->sharpness << "\n";
+#endif
+/*
+notice that the object name has changed from 'axe' to 'axWrapper'
+You don't have to do this, you can keep your current object name and just change its type to your Wrapper class
+
+ 8) After you finish, click the [run] button.  Clear up any errors or warnings as best you can.
+ 
+ see here for an example: https://repl.it/@matkatmusic/ch3p04example
+
+ you can safely ignore any warnings about exit-time-destructors.
+ if you would like to suppress them, add -Wno-exit-time-destructors to the .replit file 
+   with the other warning suppression flags
+ */
+
+
+
 
 
 #include <iostream>
