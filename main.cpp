0882a56894fc3d109b39d0c017f007c59776996b
matkatmusic <matkatmusic@gmail.com>
2020-11-26 01:00:09 -0800

    added part 4

--- main.cpp
+++ main.cpp
@@ -1,49 +1,36 @@
 /*
- Project 5: Part 3 / 4
- video: Chapter 3 Part 4: 
+ Project 5: Part 4 / 4
+ video: Chapter 3 Part 7
 
-Create a branch named Part3
-
- the 'new' keyword
-
- 1) add #include "LeakedObjectDetector.h" to main
+ Create a branch named Part4
  
- 3) Add 'JUCE_LEAK_DETECTOR(OwnerClass)' at the end of your UDTs.
+ Don't #include what you don't use
  
- 4) write the name of your class where it says "OwnerClass"
+ 1) Your task is to refactor your Chapter 3 Part 4 task into separate source and header files.
+         Add files via the pane on the left.
  
- 5) write wrapper classes for each type similar to how it was shown in the video
+ 2) Put all of your declarations for each class in .h files
+         One header file per class ( i.e. Raider.h for a class named "Raider" )
  
- 7) update main() 
-      replace your objects with your wrapper classes, which have your UDTs as pointer member variables.
-      
-    This means if you had something like the following in your main() previously: 
-*/
-#if false
- Axe axe;
- std::cout << "axe sharpness: " << axe.sharpness << "\n";
- #endif
- /*
-    you would update that to use your wrappers:
-    
+ 3) Put all of your implementations in .cpp files.
+         one cpp file per class ( i.e. Raider.cpp for a class named "Raider" )
+ 
+ 4) Put all of your Wrapper classes in a single Wrappers.h file
+         if you implemented your wrapper class functions in-class, you'll need to move them to Wrappers.cpp
+ 
+ 5) NO IN-CLASS IMPLEMENTATION ALLOWED.
+         the only exception is the existing Atomic.h and LeakedObjectDetector.h
+ 
+ 6) for every .cpp file you have to make, insert it into the .replit file after 'main.cpp'.  Don't forget the spaces between file names.
+ If you need help with this step, send me a DM.
+ 
+ 7) click the [run] button.  Clear up any errors or warnings as best you can.
+ 
+ Remember, your Chapter 3 Part 4 task worked when it was all in one file. so you shouldn't need to edit any of it.  
+         just split it up into source files and provide the appropriate #include directives.
  */
 
-#if false
-AxeWrapper axWrapper( new Axe() );
-std::cout << "axe sharpness: " << axWrapper.axPtr->sharpness << "\n";
-#endif
-/*
-notice that the object name has changed from 'axe' to 'axWrapper'
-You don't have to do this, you can keep your current object name and just change its type to your Wrapper class
 
- 8) After you finish, click the [run] button.  Clear up any errors or warnings as best you can.
- 
- see here for an example: https://repl.it/@matkatmusic/ch3p04example
-
- you can safely ignore any warnings about exit-time-destructors.
- if you would like to suppress them, add -Wno-exit-time-destructors to the .replit file 
-   with the other warning suppression flags
- */
 
 
 
