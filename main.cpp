6c0ba0cfdfa554d79df96531a24848f55b3c6235
matkatmusic <matkatmusic@gmail.com>
2021-08-22 23:18:19 -0700

    revised instructions

--- main.cpp
+++ main.cpp
@@ -4,40 +4,36 @@ Project 5: Part 1 / 4
 
  Create a branch named Part1
 
- Destructors
+Purpose:  This project continues developing Project3.
+       you will learn how to take code from existing projects and migrate only what you need to new projects
+       you will learn how to write code that doesn't leak as well as how to refactor. 
 
- If you didn't do Project3, write 3 UDTs below (where it asks for Copied UDTs) that have: 
-        member functions,
-        member variables, 
-        constructors that initialize the members. 
-        for() or while() loops that modify member variables
+ Destructors
         
- Define your implementations of all functions OUTSIDE of the class. 
- Then do instruction 2)
+ 0) move all of your implementations of all functions to OUTSIDE of the class. 
  
- If you DID do Project 3, do instruction 1)
- 
- 1) Copy a few of your user-defined types (the ones with constructors and for()/while() loops from Project 3) here
+ 1) Copy 3 of your user-defined types (the ones with constructors and for()/while() loops from Project 3) here
+    Choose the classes that contained nested classes.  Include the nested classes when you copy them over.
+
  2) add destructors
         make the destructors do something like print out the name of the class.
 
-	If you already did Chapter2 part 10 on Coliru and don't have a Project3, just grab some UDTs from your Coliru Ch2P10 submission. 
- 
- 3) add 2 new UDTs that use only the types you copied above.
- 4) Add destructors to these 2 new types that do stuff.  
+ 3) add 2 new UDTs that use only the types you copied above as member variables.
+
+ 4) add 2 member functions that use your member variables to each of these new UDTs
+
+ 5) Add constructors and destructors to these 2 new types that do stuff.  
         maybe print out the name of the class being destructed, or call a member function of one of the members.  be creative
  
- 5) Instantiate your 2 UDT's from step 4) in the main() function at the bottom.
+ 6) copy over your main() from the end of Project3 
+        get it to compile with the types you copied over.
+        remove any code in main() that uses types you didn't copy over.
  
- 6) After you finish, click the [run] button.  Clear up any errors or warnings as best you can.
+ 7) Instantiate your 2 UDT's from step 4) in the main() function at the bottom.
+       call their member functions.
  
- Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
+ 8) After you finish, click the [run] button.  Clear up any errors or warnings as best you can.
  
-Make a pull request after you make your first commit and pin the pull request link to our DM thread.
-
-send me a DM to check your pull request
-
- Wait for my code review.
  */
 
 /*
@@ -54,10 +50,26 @@ send me a DM to check your pull request
 
 /*
  new UDT 4:
+ with 2 member functions
  */
 
 /*
  new UDT 5:
+ with 2 member functions
+ */
+
+/*
+ MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
+
+ Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
+ 
+ If you didn't already: 
+    Make a pull request after you make your first commit
+    pin the pull request link and this repl.it link to our DM thread in a single message.
+
+ send me a DM to review your pull request when the project is ready for review.
+
+ Wait for my code review.
  */
 
 #include <iostream>
