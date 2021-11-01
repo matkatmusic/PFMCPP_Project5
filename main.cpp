359845e8eab9a8229e6fbfdc861839f5ddb996a7
Charles Schiermeyer <matkatmusic@gmail.com>
2021-11-01 12:47:12 -0700

    Added part 1 Instructions

--- main.cpp
+++ main.cpp
@@ -1,7 +1,83 @@
-#include <iostream>
+/*
+Project 5: Part 1 / 4
+ video Chapter 2 - Part 12
 
+ Create a branch named Part1
+
+Purpose:  This project continues developing Project3.
+       you will learn how to take code from existing projects and migrate only what you need to new projects
+       you will learn how to write code that doesn't leak as well as how to refactor. 
+
+ Destructors
+        
+ 
+ 
+ 1) Copy 3 of your user-defined types (the ones with constructors and for()/while() loops from Project 3) here
+    Choose the classes that contained nested classes.  Include the nested classes when you copy them over.
+
+ 2) move all of your implementations of all functions to OUTSIDE of the class.
+
+ 3) add destructors
+        make the destructors do something like print out the name of the class.
+
+ 4) add 2 new UDTs that use only the types you copied above as member variables.
+
+ 5) add 2 member functions that use your member variables to each of these new UDTs
+
+ 6) Add constructors and destructors to these 2 new types that do stuff.  
+        maybe print out the name of the class being destructed, or call a member function of one of the members.  be creative
+ 
+ 7) copy over your main() from the end of Project3 
+        get it to compile with the types you copied over.
+        remove any code in main() that uses types you didn't copy over.
+ 
+ 8) Instantiate your 2 UDT's from step 4) in the main() function at the bottom.
+       call their member functions.
+ 
+ 9) After you finish, click the [run] button.  Clear up any errors or warnings as best you can.
+
+ you can resolve any [-Wdeprecated] warnings by adding -Wno-deprecated to list of compiler arguments in the .replit file. all of the "-Wno" in that file are compiler arguments.
+
+ */
+
+/*
+ copied UDT 1:
+ */
+
+/*
+ copied UDT 2:
+ */
+
+/*
+ copied UDT 3:
+ */
+
+/*
+ new UDT 4:
+ with 2 member functions
+ */
+
+/*
+ new UDT 5:
+ with 2 member functions
+ */
+
+/*
+ MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
+
+ Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
+ 
+ If you didn't already: 
+    Make a pull request after you make your first commit
+    pin the pull request link and this repl.it link to our DM thread in a single message.
+
+ send me a DM to review your pull request when the project is ready for review.
+
+ Wait for my code review.
+ */
+
+#include <iostream>
 int main()
 {
     std::cout << "good to go!" << std::endl;
-    return 0;    
 }
