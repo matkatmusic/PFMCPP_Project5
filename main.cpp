e2db8f8f5a19888f53594f8c0891070dad61683f
Jean-Philippe Drecourt <jpdrecourt@gmail.com>
2023-01-10 22:01:55 +0000

    CatRentalMachine implemented

--- main.cpp
+++ main.cpp
@@ -102,7 +102,7 @@ Cat::Cat() :
 numLimbs(3),
 numTails(1),
 ageYears(3.6f),
-colour("orange"),
+colour("grey"),
 weightKg(4.1f),
 cutenessLevel(50),
 maxCroquettes(7)
@@ -450,18 +450,62 @@ void CatCyberOverlord::createEvilMachine(int numWings, int numTentacles)
  with 2 member functions
 */
 
-struct SelfSufficientCat
+struct CatRentalMachine
 {
     Cat cat;
     VendingMachine vendingMachine;
 
-    // SelfSufficientCat();
-    // ~SelfSufficientCat();
+    CatRentalMachine(Cat rentalCat);
+    ~CatRentalMachine();
 
-    void selfAdministerCroquettes(int numCroquettes);
-
-    void upgradeCroquettes(std::string newBrand, std::string newFlavour);
+    Cat distributeCat();
+    void returnCat(Cat returnedCat);
 };
+
+CatRentalMachine::CatRentalMachine(Cat rentalCat):
+cat(rentalCat)
+{
+    std::cout << "Genius idea! CatRentalMachine constructed" << std::endl;
+    std::cout << "Repurposing the Kitkat dispenser should be straightforward.\n";
+    vendingMachine.interiorTemperatureCelsius = 25.0f;
+    vendingMachine.targetTemperatureCelsius = 25.0f;
+    vendingMachine.kitkatDispenser.name = "Garfield";
+    vendingMachine.kitkatDispenser.flavour = cat.colour;
+    vendingMachine.kitkatDispenser.inventory = 1;
+    vendingMachine.kitkatDispenser.priceEuros = 50.0f;
+    std::cout 
+        << "New parameters: \n"
+        << std::setprecision(1)
+        << "Temperature: " << vendingMachine.interiorTemperatureCelsius << "\n"
+        << "Product name: " << vendingMachine.kitkatDispenser.name << "\n"
+        << "Product flavour: " << vendingMachine.kitkatDispenser.flavour << "\n"
+        << std::setprecision(2)
+        << "Product price: " <<     vendingMachine.kitkatDispenser.priceEuros << std::endl;
+}
+
+CatRentalMachine::~CatRentalMachine()
+{
+    std::cout << "The authorities get wind of this lucrative business. CatRentalMachine destructed" << std::endl;
+}
+
+Cat CatRentalMachine::distributeCat()
+{
+    std::cout << "Distributing a cat for rental\n";
+    std::cout << "His name is " << vendingMachine.kitkatDispenser.name << "\n";
+    return cat;
+}
+
+void CatRentalMachine::returnCat(Cat returnedCat)
+{
+    if (returnedCat.colour != cat.colour)
+    {
+        std::cout << "You are trying to return the wrong cat!" << std::endl;
+    }
+    else
+    {
+        std::cout << "Cat safely returned! Thanks for using our services!" << std::endl;
+    }
+}
 
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
@@ -569,13 +613,23 @@ int main()
     catCyberOverlord.createEvilMachine(2, 6);
     std::cout << "Who needs wings? 6 tentacles will do!\n";
     catCyberOverlord.createEvilMachine(0, 6);
-    
-    
-
     std::cout << std::endl;
     
-    SelfSufficientCat selfSufficientCat;
-
+    Cat rentalCat;
+    rentalCat.numLimbs = 4;
+    rentalCat.cutenessLevel = 150;
+    rentalCat.maxCroquettes = 12345;
+    rentalCat.colour = "orange";
+    CatRentalMachine catRentalMachine(rentalCat);
+    Cat myRentalCat = catRentalMachine.distributeCat();
+    std::cout << "What a beautiful cat!\n";
+    std::cout << std::fixed << std::setprecision(1)
+        << "The cat has " << myRentalCat.numLimbs << " limb(s) and " << myRentalCat.numTails << " tail(s). It is " << myRentalCat.ageYears << " year(s) old. Its colour is " << myRentalCat.colour << ". And its cuteness level is " << myRentalCat.cutenessLevel << ".\n"
+        << "Cat weight: " << myRentalCat.weightKg << "kg\n"
+        << "Max croquettes eaten in one sitting: " << myRentalCat.maxCroquettes << "\n"
+        << "This cat is too cute, I'll return a grey cat instead....\n";
+    catRentalMachine.returnCat(cat);
+    
     std::cout << std::endl;
     std::cout << "good to go!" << std::endl;
 }
