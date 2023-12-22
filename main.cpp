adfcd1246060c9bd6835ca51f150bec423b57c9a
odagonzpfm1 <154253425+odagonzpfm1@users.noreply.github.com>
2023-12-22 16:21:17 +0000

    initial commit

--- main.cpp
+++ main.cpp
@@ -5,8 +5,9 @@ Project 5: Part 1 / 4
  Create a branch named Part1
 
 Purpose:  This project continues developing Project3.
-       you will learn how to take code from existing projects and migrate only what you need to new projects
-       you will learn how to write code that doesn't leak as well as how to refactor. 
+       you will learn how to take code from existing projects and migrate only
+what you need to new projects you will learn how to write code that doesn't leak
+as well as how to refactor.
 
 NOTE: there are 2 sets of instructions:
        One for if you completed project 3
@@ -14,11 +15,12 @@ NOTE: there are 2 sets of instructions:
 
  Destructors
 
-===============================================================     
+===============================================================
  If you completed Project 3:
- 
- 1) Copy 3 of your user-defined types (the ones with constructors and for()/while() loops from Project 3) here
-    Choose the classes that contained nested classes.  Include the nested classes when you copy them over.
+
+ 1) Copy 3 of your user-defined types (the ones with constructors and
+for()/while() loops from Project 3) here Choose the classes that contained
+nested classes.  Include the nested classes when you copy them over.
 
  2) move all of your implementations of all functions to OUTSIDE of the class.
 
@@ -26,22 +28,19 @@ NOTE: there are 2 sets of instructions:
         make the destructors do something like print out the name of the class.
 ===============================================================
 If you skipped Project 3:
-write 3 UDTs below that EACH have: 
+write 3 UDTs below that EACH have:
         5 member variables
-            the member variable names and types should be relevant to the work the UDT will perform.
-            pick properties that can be represented with 'int float double bool char std::string'
-        3 member functions with an arbitrary number of parameters
-            give some of those parameters default values.
-        constructors that initialize some of these member variables
-            the remaining member variables should be initialized in-class
-        for() or while() loops that modify member variables
- 1) 2 of your 3 UDTs need to have a nested UDT.
-    this nested UDT should fulfill the same requirements as above:
-        5 member variables  
-        3 member functions
-        constructors and loops.
-        
- 2) Define your implementations of all functions OUTSIDE of the class. 
+            the member variable names and types should be relevant to the work
+the UDT will perform. pick properties that can be represented with 'int float
+double bool char std::string' 3 member functions with an arbitrary number of
+parameters give some of those parameters default values. constructors that
+initialize some of these member variables the remaining member variables should
+be initialized in-class for() or while() loops that modify member variables 1) 2
+of your 3 UDTs need to have a nested UDT. this nested UDT should fulfill the
+same requirements as above: 5 member variables 3 member functions constructors
+and loops.
+
+ 2) Define your implementations of all functions OUTSIDE of the class.
  NO IN-CLASS IMPLEMENTATION ALLOWED
  3) add destructors to all of your UDTs
         make the destructors do something like print out the name of the class.
@@ -49,22 +48,28 @@ write 3 UDTs below that EACH have:
 
  4) add 2 new UDTs that use only the types you copied above as member variables.
 
- 5) add 2 member functions that use your member variables to each of these new UDTs
+ 5) add 2 member functions that use your member variables to each of these new
+UDTs
 
- 6) Add constructors and destructors to these 2 new types that do stuff.  
-        maybe print out the name of the class being destructed, or call a member function of one of the members.  be creative
- 
- 7) copy over your main() from the end of Project3 
+ 6) Add constructors and destructors to these 2 new types that do stuff.
+        maybe print out the name of the class being destructed, or call a member
+function of one of the members.  be creative
+
+ 7) copy over your main() from the end of Project3
         get it to compile with the types you copied over.
         remove any code in main() that uses types you didn't copy over.
- 
+
  8) Instantiate your 2 UDT's from step 4) in the main() function at the bottom.
        call their member functions.
- 
- 9) After you finish, click the [run] button.  Clear up any errors or warnings as best you can.
 
- you can resolve any [-Wdeprecated] warnings by adding -Wno-deprecated to list of compiler arguments in the .replit file. all of the "-Wno" in that file are compiler arguments.
- You can resolve any [-Wpadded] warnings by adding -Wno-padded to the list of compiler arguments in the .replit file. all of the "-Wno" in that file are compiler arguments.
+ 9) After you finish, click the [run] button.  Clear up any errors or warnings
+as best you can.
+
+ you can resolve any [-Wdeprecated] warnings by adding -Wno-deprecated to list
+of compiler arguments in the .replit file. all of the "-Wno" in that file are
+compiler arguments. You can resolve any [-Wpadded] warnings by adding
+-Wno-padded to the list of compiler arguments in the .replit file. all of the
+"-Wno" in that file are compiler arguments.
 
  Ignore the Atomic.h and LeakedObjectDetector.h files for now.
  You will use those in Part 3 of this project.
@@ -96,11 +101,13 @@ write 3 UDTs below that EACH have:
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
 
- Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
- 
- If you didn't already: 
+ Commit your changes by clicking on the Source Control panel on the left,
+ entering a message, and click [Commit and push].
+
+ If you didn't already:
     Make a pull request after you make your first commit
-    pin the pull request link and this repl.it link to our DM thread in a single message.
+    pin the pull request link and this repl.it link to our DM thread in a single
+ message.
 
  send me a DM to review your pull request when the project is ready for review.
 
@@ -108,7 +115,44 @@ write 3 UDTs below that EACH have:
  */
 
 #include <iostream>
-int main()
-{
-    std::cout << "good to go!" << std::endl;
+#include <stdlib.h>
+
+struct Student {
+  int grade;
+  std::string name;
+  int courseID;
+  int age;
+  int studentID;
+  Student(std::string givenName) {
+    grade = 100;
+    name = givenName;
+    courseID = 5;
+    age = 18;
+    studentID = 123456;
+  }
+  ~Student() {}
+  void printStudentID();
+  void printStudentName();
+  void printStudentGrade();
+};
+
+void Student::printStudentID() {
+  std::cout << "Student ID: " << studentID << std::endl;
+}
+
+void Student::printStudentName() {
+  std::cout << "Student Name: " << name << std::endl;
+}
+
+void Student::printStudentGrade() {
+  std::cout << "Student Grade: " << grade << std::endl;
+}
+
+int main() {
+  Student mario = Student("Mario");
+  mario.printStudentName();
+  mario.printStudentID();
+  mario.printStudentGrade();
+
+  std::cout << "good to go!" << std::endl;
 }
